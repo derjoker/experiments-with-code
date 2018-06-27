1876fbd0fd801bfb95176abc3e165e3bb2bdeba9
Feng Ya <fengya09@gmail.com>
2018-06-27 19:33:38 +0800

    prototype vs. __proto__

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ydkjs-code
-You Don't Know JS code example / experiment
+
+Code example / experiment (You Don't Know JS).
+
+## 猜想
+
+- `Object.create` -> `__proto__` vs. `new` -> `prototype`
--- this & object prototypes/chapter5.js
+++ this & object prototypes/chapter5.js
@@ -16,6 +16,8 @@ Object.defineProperties(obj1, {
 });
 
 var obj2 = Object.create(obj1);
+// Object.create将obj1置于obj2的原型链`__proto__`之中
+console.log(obj1, obj2);
 
 obj2.a = "a2";
 obj2.b = "b2";
@@ -46,4 +48,70 @@ for (let key of ["a", "b", "c", "d"]) {
 
 // obj2从obj1来(Object.create)，即obj1在obj2的原型链上
 // 1. 操作obj1不会影响obj2，操作obj2不会影响obj1
-// 2. 原型链上(obj1)出现`writable: false` (c)或者setter (d)，该`property`在obj1和obj2上都不能被改变/赋值，但是可以通过defineProperty改变
+// 2. 原型链上(obj1)出现`writable: false` (c)或者setter (d)，该`property`在obj1和obj2上都不能通过赋值`=`改变，但是可以通过defineProperty改变
+
+// "Class"
+
+function Foo() {}
+var foo = new Foo();
+
+console.log(Foo, foo);
+console.log(Object.getPrototypeOf(foo) === Foo.prototype);
+console.log(Foo.prototype.constructor === Foo);
+
+console.log(Foo.constructor); // Function
+console.log(foo.constructor); // Foo
+
+// A -copy-> B, A <-link- B (delegate)
+// 方向
+
+function Bar() {}
+console.log(Bar.prototype);
+Bar.prototype = {
+  b: "b",
+  a: "a",
+  r: "r"
+};
+var bar = new Bar();
+
+console.log(Bar, bar);
+console.log(Object.getPrototypeOf(bar));
+
+console.log(Bar.constructor); // Function
+console.log(bar.constructor); // Object
+
+// "(Prototypal) Inheritance"
+
+function Foo(name) {
+  this.name = name;
+}
+
+Foo.prototype.myName = function() {
+  return this.name;
+};
+
+function Bar(name, label) {
+  Foo.call(this, name);
+  this.label = label;
+}
+
+// Bar.prototype = Foo.prototype; // NO!!!
+// Bar.prototype成了Foo.prototype的又一个引用/别称
+// 任何在Bar.prototype上的更改实际发生在Foo.prototype上
+
+Bar.prototype = Object.create(Foo.prototype); // YES!!!
+// 将Foo.prototype置于Bar.prototype的原型链`__proto__`中
+// 任何对Bar.prototype的更改并不会影响Foo.prototype
+// 同时保留Bar.prototype通过原型链对Foo.prototype的引用
+
+// Object.setPrototypeOf(Bar.prototype, Foo.prototype); // ES6+
+
+console.log(Foo, Bar);
+
+Bar.prototype.myLabel = function() {
+  return this.label;
+};
+
+var bar = new Bar("bar", "label bar");
+console.log(bar.myName());
+console.log(bar.myLabel());
